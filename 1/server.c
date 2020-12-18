1a47579bfdf3e0497db9940088574d0b6b614b32
megnar <52637480+megnar@users.noreply.github.com>
2020-12-19 02:22:11 +0300

    Update server.c

--- 1/server.c
+++ 1/server.c
@@ -6,72 +6,127 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 
-#define path "/home/podles/Desktop/proga/1/F.fifo"
 #define part_path "/home/podles/Desktop/proga/1/"
 #define max_file_size 4096
+#define N 256
+void *p[4];
+char dir[N];
+void my_handler(int sig){
+	free(p[0]);
+	free(p[1]);
+	free(p[2]);
+	free(p[3]);
+	sprintf(dir, "%s/FIFO.fifo", dir);
+	if (remove (dir) == -1){
+		printf("remove error");
+		exit(errno);
+		}	
+	exit(0);
+	
+}
 
 int main(int argc, char * argv[]){
-	if (argc != 2){
-		printf("arguments error\n");
-		exit(-1);
+	signal(SIGINT, my_handler);
+	
+	if (getcwd(dir, N) == NULL) {
+		printf("getcwd error\n");
+		exit(errno);
 	}
+	char path[N];
+	
+	sprintf(path, "%s/FIFO.fifo", dir);
 
-
-	char *data;
-	data = (char *) malloc (50*sizeof(char));
-	char *file;
-	file = (char *) malloc (50*sizeof(char));
-	char * text;
-	text = (char *) malloc (max_file_size * sizeof (char));
-	int fd_r, fd_w;
-
-	fd_w = open (path, O_WRONLY|O_NONBLOCK);
-	if ( fd_w == -1 ){
-		printf("open fifo error\n");
-		exit (errno);
-	}
-	pid_t id;
-	id = getpid();
-	sprintf(data, "%d;%s", id, argv[1]);
-	if ( write(fd_w, data, 50) == -1 ){
-		printf("write in fifo error\n");
-		exit (errno);
-	}
-	close(fd_w);
-	char new_path[50];
-	sprintf(new_path, "%s/client%d.fifo", part_path, id);
-
-	if ( mkfifo (new_path, 0600) == -1){
+	if ( mkfifo (path, 0600) == -1){
 		if (errno != EEXIST){
 			printf("create fifo error\n");
 			exit (errno);
 		}
 	}
 
-	fd_r = open (new_path, O_RDONLY);
-	if ( fd_r == -1 ){
-		printf("open fifo error\n");
+	int fd_ser, fd_cl, fd;
+	char *data = (char *) malloc (N*sizeof(char));
+	p[0] = (void *) data; 
+	char *file = (char *) malloc (N*sizeof(char));
+	p[1] = (void *) file; 
+	struct stat *about_file = (struct stat *) malloc (sizeof(struct stat));
+	p[2] = (void *) about_file; 
+	char * buf;
+	buf = (char *) malloc (max_file_size * sizeof (char));
+	if (buf == NULL){
+		printf("buf malloc error\n");
+		exit(errno);
+	}
+	p[3] = (void *) buf;
+	fd_ser = open (path, O_RDWR);
+	if ( fd_ser == -1 ){
+		printf("open fifo1 error\n");
 		exit (errno);
 	}
 
+
 	while(1){
-		int rd =  read(fd_r, text, max_file_size);
-		if (rd == -1){
-			printf("write in fifo error\n");
+
+	
+
+		if( read(fd_ser, data, N)  == -1){
+				printf("%s\n", data);
+				printf("read from fifo error\n");
+				exit (errno);
+			}	
+
+		file = strchr (data, ';');
+		*file = '\0';
+		file++;
+		char new_path[N];
+		sprintf(new_path, "%s/client%s.fifo",dir, data);
+
+		fd = open(file, O_RDONLY);
+		if ( fd == -1 ){
+			printf("open file error\n");
 			exit (errno);
 		}
-		if (rd < max_file_size)
-			break;
-		if ((write(1, text, rd)) == -1) {
-			printf("write error\n");
+		if ( mkfifo (new_path, 0600) == -1){
+			if (errno != EEXIST){
+				printf("create fifo error\n");
+				exit (errno);
+			}
+		}
+
+		if ( stat(file, about_file) == -1){
+			printf("stat error\n");
 			exit(errno);
 		}
+		long int file_size = about_file->st_size;
+		int i;
 		
-	}
-	close(fd_r);
-	printf("%s", text);// прикол 
+		fd_cl = open(new_path, O_WRONLY);
+		if ( fd_cl == -1 ){
+			printf("open fifo error\n");
+			exit (errno);
+		}
+		while (1){
+			int rd = read(fd, buf, max_file_size);
+			if( rd  == -1){
+				printf("read from file error\n");
+				exit (errno);
+			}	
+			if (write(fd_cl, buf, rd) == -1){
+				printf("write in fifo error\n");
+				exit (errno);
+			}
+			if( rd < max_file_size){
 
-	free(data);
-	free(file);
+				break;
+			}			
+		}
+
+		if (remove (new_path) == -1){
+			printf("remove error");
+			exit(errno);
+		}	
+	}	
+		
+	
 }
