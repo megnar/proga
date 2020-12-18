ff277ede51ff4005eb80746f9a4bd599a2badb72
megnar <52637480+megnar@users.noreply.github.com>
2020-12-19 02:25:06 +0300

    Update server.c

--- 2/server.c
+++ 2/server.c
@@ -9,38 +9,46 @@
 #include <sys/sem.h>
 #include <errno.h>
 
-#define path "/home/podles/Downloads/newproc.c"
+
 #define pathname "/home/podles/Downloads/serv.c"
 
 #define page_size 4096
 #define N 256
 #define size 13
-
+char dir[N];
 int main(int argc, char *argv[])
 {	
+	
+	if (getcwd(dir, N) == NULL) {
+		printf("getcwd error\n");
+		exit(errno);
+	}
+	
+	char path[N];
+	
+	sprintf(path, "%s/file.txt", dir);
 	open(path, O_CREAT);
         key_t shm_key, shm_key1;
 	struct sembuf my_sem[4];
         if ( (shm_key = ftok(path, 0)) == -1){
 		printf("ftok error1\n");
 		exit (errno);
-       	}
-	if ( (shm_key1 = ftok(pathname , 0)) < 0){
+        }
+	if ( (shm_key1 = ftok(path , 1)) < 0){
                 printf("ftok error2\n");
                 exit (errno);
         }
 
-       	int shm_id;
-       	if ( (shm_id =  shmget(shm_key, 4096, IPC_CREAT|0666)) == -1){
+        int shm_id;
+        if ( (shm_id =  shmget(shm_key, 4096, IPC_CREAT|0666)) == -1){
 		printf("shmget error\n");
 		exit(errno);
-       	}
-
-       	char *buf;
+	}
+	char *buf;
         if ( (buf = (char *) shmat (shm_id, NULL, 0)) == (char *)(-1) ){
-       		printf("shmat error\n");
+       	printf("shmat error\n");
 		exit(errno);
-       	}
+	}
 	int sem_id;
 	if ( (sem_id =  semget(shm_key1, 3, IPC_CREAT|0666)) < 0){
 		perror("semget");
@@ -87,15 +95,15 @@ int main(int argc, char *argv[])
        		}
 
 //	printf("sem1 %d,sem 2 %d ", (semctl(sem_id, 0, GETVAL, 0 )), (semctl(sem_id, 1, GETVAL, 0 )));
-      		int need_size = 0;
-		int two_power = 1;
+      		int razm = 0;
+		int x = 1;
 		for (int i = 1; i <= size; i++){
 			if ( *(buf+size-i) == '1'){
-				need_size += two_power;//считаем требуемый размер файла
+				razm += x;//считаем требуемый размер файла
 			}
-			two_power = two_power * 2;
+			x = x * 2;
 		}
-		if ((write(1, buf+size, need_size)) == -1) {//
+		if ((write(1, buf+size, razm)) == -1) {//
 			printf("write error\n");
 			exit(errno);
 		}
@@ -110,4 +118,3 @@ int main(int argc, char *argv[])
 
 	}
 }
-
